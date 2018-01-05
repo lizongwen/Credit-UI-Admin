7ff7548d19b12cee623413771c9beb0204f03b0c
lizongwen <93700983@qq.com>
2018-01-05 18:36:28 +0800

    1

--- assets/js/backend/general.js
+++ assets/js/backend/general.js
@@ -1,7 +1,9 @@
-define(['jquery', 'bootstrap'], function ($, undefined) {
+define(['jquery', 'bootstrap' ,'bootstrap-select'], function ($, undefined) {
 	var Controller = {
 		index:function(){
-			
+			$('.selectpicker').each(function(){
+				$(this).selectpicker()
+			})
 		}
 	}
 	return Controller
